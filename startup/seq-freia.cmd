7731635d46d1004d74cd3825e24318f570103ba2
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2018-05-23 10:37:40 +0200

    Fixed extracting data from $(P):ADC:sRdV.

--- startup/seq-freia.cmd
+++ startup/seq-freia.cmd
@@ -1,7 +1,7 @@
 epicsEnvSet("AUTOSAVE_SYSM_PV_PREFIX","seq:")
 dbLoadRecords("seq-freia-gen.db","P=$(P)")
 dbLoadRecords("seq-freia.db","P=$(P)")
-dbLoadRecords("freia-seq-cRIO-ADC.db","P=$(P), INDX=2, NAME=Cav1-Coupl:MultipactDet:I, EGU=mA, LINR=LINEAR, LOPR=0.0, HOPR=10.0, EOFF=0.0, ESLO=0.001")
+dbLoadRecords("seq-freia-cRIO-ADC.db","P=$(P), INDX=2, NAME=Cav1-Coupl:MultipactDet:I, EGU=mA, LINR=LINEAR, LOPR=0.0, HOPR=10.0, EOFF=0.0, ESLO=0.001")
 
 
 requireSnippet(seq-freia-preSaveRestore.cmd)
