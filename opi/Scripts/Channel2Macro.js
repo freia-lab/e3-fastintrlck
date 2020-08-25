ca3013e13b0f169107b997595632ff579f65994e
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2020-08-25 19:36:20 +0200

    Restored deleted (by mistake?) file Channel2Macro.js.

---
+++ opi/Scripts/Channel2Macro.js
@@ -0,0 +1,8 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+var macroInput = DataUtil.createMacrosInput(true);
+var pv = PVUtil.getString(pvArray[0]);
+
+macroInput.put("Channel2Macro", pv);
+widgetController.setPropertyValue("macros", macroInput);
+
